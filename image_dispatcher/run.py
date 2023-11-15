15d55ab7940d04a75016703812d1c4018ecb486c
François Weber <francois.weber.commercial@gmx.fr>
2023-11-15 14:16:09 +0100

    [FEA] Actually moved pics and kept track of moves

--- image_dispatcher/run.py
+++ image_dispatcher/run.py
@@ -1,13 +1,15 @@
-from hashlib import sha256
-from typing import List
-import streamlit as st
 import glob
+import os
+from datetime import datetime
+from hashlib import sha256
+from os import path as osp
+from shutil import move
+from typing import List
+
+import pandas as pd
+import streamlit as st
 from PIL import Image, ImageOps
 from pillow_heif import register_heif_opener
-from os import path as osp
-from collections import defaultdict
-import os
-import pandas as pd
 
 st.set_page_config(layout="wide")
 register_heif_opener()
@@ -15,38 +17,10 @@ register_heif_opener()
 IMG_FORMATS_LOWER = {".jpeg", ".jpg", ".heic", ".heif", ".png"}
 SIZE_INF_SURROGATE = 100000
 MAX_SIZE = 300
+MAIN_DIR = "/Users/f.weber/Pictures/Photos du téléphone"
 
-
-@st.cache_resource
-def collect_images2(limit=-1):
-    img_main_dir = "/Users/f.weber/Pictures/Photos du téléphone"
-    img_glob = glob.glob(osp.join(img_main_dir, "**", "*"), recursive=True)
-
-    img_exts = defaultdict(list)
-    img_paths = []
-    for path in img_glob:
-        if not osp.isfile(path):
-            continue
-        path_, ext_ = osp.splitext(path)
-        img_exts[path_].append(ext_)
-        if ext_.lower() in IMG_FORMATS_LOWER:
-            img_paths.append(path)
-    img_paths = img_paths[:limit]
-
-    img_thumbs = []
-    for path in img_paths:
-        pic = Image.open(path)
-        (h, w) = (pic.height, pic.width)
-        if w >=h :
-            pic.thumbnail((SIZE_INF_SURROGATE, MAX_SIZE))
-        else:
-            pic.thumbnail((MAX_SIZE, SIZE_INF_SURROGATE))
-        pic.thumbnail((SIZE_INF_SURROGATE, 200))
-        img_thumbs.append(pic)
-
-    df = pd.DataFrame(data={"path": img_paths, "thumbnail": img_thumbs, "processed": False})
-    df["key"] = df.path.map(lambda path: sha256(path.encode()).hexdigest()[:16])
-    return df
+if "run_id" not in st.session_state:
+    st.session_state.run_id = datetime.now().strftime("%y-%m-%dT%H-%M")
 
 def first_usable_path(paths):
     paths_ = paths.to_list()
@@ -69,16 +43,17 @@ def load_thumb(path):
     return pic
 
 @st.cache_resource
-def collect_images(limit=-1):
-    img_main_dir = "/Users/f.weber/Pictures/Photos du téléphone"
-    img_glob = glob.glob(osp.join(img_main_dir, "**", "*"), recursive=True)
+def collect_images(limit=32):
+    # img_main_dir = "/Users/f.weber/Pictures/Photos du téléphone"
+    img_glob = glob.glob(osp.join(MAIN_DIR, "**", "*"), recursive=True)
     img_paths = [path for path in img_glob if osp.isfile(path)]
     df = pd.DataFrame(data={"path": img_paths})
     df["basename"] = df.path.map(lambda path: osp.splitext(path)[0])
     df["ext"] = df.path.map(lambda path: osp.splitext(path)[1])
     # now make sure we'll be able to move every side-file
     df = df.groupby("basename").agg({"ext": list, "path": first_usable_path}).reset_index()
-    df = df.sample(32)
+    if limit > 0:
+        df = df.iloc[:limit]
     # attach further info
     df["thumbnail"] = df.path.map(load_thumb)
     df["processed"] = False
@@ -96,14 +71,25 @@ def select_dest(prefix):
     return [d for d in get_directories() if prefix.lower() in d.lower()]
 
 
+
 def process_batch():
+    # retrieve selected images
     clicked_key = [k for k, v in st.session_state.items() if k.endswith("+checkbox") and v]
+    if len(clicked_key) == 0:
+        return
     clicked_img_key = [k.split("+")[0] for k in clicked_key]
-    st.session_state.df.loc[st.session_state.df.key.isin(set(clicked_img_key)), "processed"] = True
-    for key in clicked_img_key:
-        st.text(f"mv {key} to {st.session_state.dest}")
-        st.session_state[key + "+disabled"] = True
-        st.session_state[key + "+checkbox"] = False
+    selected_keys = st.session_state.df.key.isin(set(clicked_img_key))
+    # mark them as done in df
+    st.session_state.df.loc[selected_keys, "processed"] = True
+    st.session_state.df.loc[selected_keys, "dst"] = st.session_state.dest
+    # proceed to mv
+    df_mv = st.session_state.df.loc[selected_keys, ["basename", "ext", "dst"]].explode("ext")
+    for datum in df_mv.to_dict(orient="records"):
+        src = datum["basename"] + datum["ext"]
+        # st.text(f"mv {src} to {st.session_state.dest}")
+        move(src, st.session_state.dest)
+    df_mv.to_csv(osp.join(MAIN_DIR, f"{st.session_state.run_id}.csv"))
+
 
 
 st.title("Dispatch images")
@@ -175,7 +161,6 @@ with st.form(key="aa"):
         for col, im in zip(cols, im_row):
             with col:
                 st.image(ImageOps.grayscale(im["thumbnail"]))
-                # st.checkbox("select", key=im["key"] + "+checkbox")
 
 
 
